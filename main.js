872a769f854ecc11e9e2e13606b7cd356bf74bc0
gitgibbo <mattgibbons@me.com>
2022-03-26 22:27:23 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // Hello, thanks for looking at this portfolio project!
+// This is a haiku generator...
+
+
+// I Will store the components of the message as an object to keep the code neat and readable.
+const messageObj = {
+    opening: ['hello', 'hiya', 'greetings'],
+    middle: ['sunshine', 'beautiful'],
+    closing: ['you are pretty', 'i love you'],
+};
+
+// This is the function which concats randomly picked elements out of the arrays of a message obj
+const messageBuilder = obj => {
+    let msg = `${obj.opening[Math.floor(Math.random()*obj.opening.length)]}\n${obj.middle[Math.floor(Math.random()*obj.middle.length)]},\n${obj.closing[Math.floor(Math.random()*obj.closing.length)]}.`;
+    return msg;
+}
+
+console.log(messageBuilder(messageObj));
